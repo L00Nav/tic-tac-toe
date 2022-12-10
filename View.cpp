49f69023e47286ad69410372ec3e442943cc2684
TayakiNoTenshi <101095244+TayakiNoTenshi@users.noreply.github.com>
2022-12-10 11:25:43 +0200

    Add project files.

---
+++ View.cpp
@@ -0,0 +1,21 @@
+#include "View.h";
+
+View::View()
+{
+
+}
+
+View::~View()
+{
+
+}
+
+void View::testView(void)
+{
+	cout << "butt" << endl;
+}
+
+void View::clear(void)
+{
+	system("cls");
+}
---
+++ View.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <conio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class View
+{
+public:
+	View();
+	~View();
+	void testView(void);
+	void clear(void);
+};
